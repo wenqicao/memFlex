a95672c73d687f72d02fb927f959f0d8130ca36c
qzhang90 <qzhang90@gatech.edu>
2016-07-16 23:09:57 -0400

    Create mapper.h

---
+++ core/mapper.h
@@ -0,0 +1,8 @@
+#ifndef __MAPPER_H__
+#define __MAPPER_H__
+
+unsigned long *init_mapper(size_t size);
+int insert_mapper(unsigned long *mapper, unsigned long offset);
+unsigned long get_offset(unsigned long *mapper, unsigned long offset);
+
+#endif

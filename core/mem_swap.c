56094713c74179a55ff9de4065f5ca11b83aa92a
qzhang90 <qzhang90@gatech.edu>
2016-07-20 22:16:36 -0400

    Update mem_swap.c

--- core/mem_swap.c
+++ core/mem_swap.c
@@ -2,11 +2,12 @@
 #include "mapper.h"
 #include <linux/swapfile.h>
 
-//#define SHM_DUMP
+extern void end_swap_bio_read(struct bio *bio, int err);
+#define SHM_DUMP
 //#define PROACTIVE_SWAP
 #define OFFSET_1 1048576 //4G from the start of the shm
 #define OFFSET_2 1310720 //5G from the start of the shm
-
+#define DUMP_PERCENT 1
 
 extern void __iomem * Nahanni_mem;
 extern spinlock_t swap_lock;
@@ -17,9 +18,13 @@ static DECLARE_WAIT_QUEUE_HEAD(swap_thread_wait);
 struct task_struct *mempipe_dump_shm_thread;
 
 static int all_pages_dumped = 1;
-static int dump_thread_finish = 0;
+static int dump_thread_running = 0;
 
-unsigned long memswap_size = (1<<20); /*# of pages*/
+static int init = 0;
+struct dump_args *dump_args;
+
+unsigned long memswap_size_total = (1<<20); /*# of pages*/
+unsigned long memswap_size = (1<<15); /*# of pages*/
 
 /*
 * each memswap area consists of one or more chunks
@@ -30,12 +35,12 @@ size_t init_chunks = 8;
 
 int memswap_init(struct swap_info_struct *sis){
 	
-	struct dump_args *dump_args;
 	int ret = 0;
 	unsigned long offset;/*In terms of pages*/
 	char *start;
 	
 	if(Nahanni_mem == NULL){
+		printk(KERN_ERR "Nahanni_mem = NULL\n");
 		return -1;
 	}
 	
@@ -59,17 +64,15 @@ int memswap_init(struct swap_info_struct *sis){
 	start = (char *)Nahanni_mem + offset*PAGE_SIZE;
 	memset((char *)start, '0', memswap_size*PAGE_SIZE);	
 
-	offset += memswap_size;
+	offset += memswap_size_total;
 	*(unsigned long *)Nahanni_mem = offset;
 	
 	sis->shm = start;
-	sis->shm_start = sis->shm_end = 1;
+	sis->shm_start = sis->shm_end = 0;
 	sis->disk_start = 0; 
 	sis->disk_end = 0;
 	sis->mask = 1;
 
-	memset((char *)sis->shm + OFFSET_1*PAGE_SIZE, '0', 1<<10);
-	
 	gsis = sis;
 	
 	pages_to_dump = pages_dumped = 0;
@@ -78,8 +81,8 @@ int memswap_init(struct swap_info_struct *sis){
 	if(dump_args) {
 		dump_args->sis = sis;
 	}
-
 	mempipe_dump_shm_thread = kthread_run(mempipe_dump_shm, dump_args, "mempipe_dump");
+	printk("before init return, mapper = %p\n", sis->mapper);
 	return ret;
 }
 
@@ -127,6 +130,7 @@ void end_dump_page(struct bio *bio, int err)
 	pages_dumped++;	
 
 	if(pages_dumped == pages_to_dump) {
+		dump_thread_running = 0;
 		pages_dumped = pages_to_dump = 0;
 		all_pages_dumped = 1;
 		wake_up_all(&swap_thread_wait);
@@ -134,14 +138,24 @@ void end_dump_page(struct bio *bio, int err)
 }
 
 
-static int dump_shm_should_run(void)
-{
-	return dump_thread_finish == 1;
+int shm_almost_full(void){
+	//return ((gsis->shm_end - gsis->shm_start) >= memswap_size*DUMP_PERCENT);
+	return ((gsis->disk_end - gsis->disk_start) >= memswap_size*DUMP_PERCENT);
 }
 
-static int swap_should_run(void)
-{
-	return (gsis->shm_end - gsis->shm_start < TOTAL_SWAP_SHM);
+int shm_is_not_full(void){
+	//return (gsis->shm_end - gsis->shm_start) < memswap_size;
+	return (gsis->disk_end - gsis->disk_start) < memswap_size;
+}
+
+int dump_shm_should_run(void){
+	return shm_almost_full();
+}
+
+int swap_should_run(void){
+	//return shm_is_not_full();
+	//dump_thread_running == 0;
+	return gsis->disk_start == gsis->disk_end;
 }
 
 int mempipe_dump_shm(void *args)
@@ -155,15 +169,28 @@ int mempipe_dump_shm(void *args)
 
 
 	while(!kthread_should_stop()) {
-		wait_event_interruptible(dump_thread_wait, dump_shm_should_run() || kthread_should_stop());
-
+		
 		if(kthread_should_stop())
 			break;
+	
+		printk("prepare to start shm_dump\n");	
+		wait_event_interruptible(dump_thread_wait, dump_shm_should_run());
+		printk("start shm_dump\n");
+		dump_thread_running = 1;
+		
+		/***/
+		sis->disk_start = sis->disk_end;
+		dump_thread_running = 0;
+		wake_up_all(&swap_thread_wait);
+		printk("finish shm_dump, wake up swap thread\n");
+		schedule();
+		/***/
 
+		/*
 		all_pages_dumped = 0;	
 
-		start = sis->shm_start;
-		end = sis->shm_end;
+		start = dump_args->start;
+		end = dump_args->end;
 		pages_to_dump = end - start;
 
 		for(offset = start; offset < end; offset++) {
@@ -175,7 +202,7 @@ int mempipe_dump_shm(void *args)
 			lock_page(page);
 
 			tmp_offset = offset;
-			memcpy(page_address(page), (char *)Nahanni_mem + tmp_offset*PAGE_SIZE, PAGE_SIZE);
+			memcpy(page_address(page), (char *)gsis->shm + tmp_offset*PAGE_SIZE, PAGE_SIZE);
 	
 			bio = bio_alloc(GFP_NOIO, 1);
 			if(bio) {
@@ -197,8 +224,7 @@ int mempipe_dump_shm(void *args)
 			unlock_page(page);
 			submit_bio(WRITE, bio);
 		}
-		
-		dump_thread_finish = 0;
+		*/
 	}
 	return ret;
 }
@@ -211,13 +237,12 @@ void (*end_write_func)(struct bio *, int))
         struct bio *bio;
         int ret = 0, rw = WRITE;
 
-	
         struct swap_info_struct *sis = page_swap_info(page);
         //Added by Qi Zhang
-        pgoff_t offset;
+	pgoff_t offset;
         swp_entry_t entry;
-	char *mdata_exist;
-        //End
+	//char *mdata_exist;
+	//End
 
 
         if (sis->flags & SWP_FILE) {
@@ -265,19 +290,31 @@ void (*end_write_func)(struct bio *, int))
         //Added by Qi Zhang
         entry.val = page_private(page);
         offset = swp_offset(entry);
-	if(sis->mapper == NULL) {
+	if(init == 0){
+		printk("start memswap_init()\n");
 		ret = memswap_init(sis); 
+		init = 1;
 	}
 
-	BUG_ON(sis->shm == NULL);
-
+	BUG_ON(sis->mapper == NULL);
 
 #ifdef SHM_DUMP
+	/*
+	printk("writepage: offset = %ld, gsis->shm_start = %ld, gsis_shm_end = %ld, memswap_size = %ld, dump_thread_running = %ld, dump_shm_should_run() = %d\n", 
+			offset, 
+			gsis->shm_start,
+			gsis->shm_end,
+			memswap_size,
+			dump_thread_running,
+			dump_shm_should_run());
+        }
+	*/
 	if(offset < sis->shm_start){
+		printk("******This swap slot is on the disk\n");
                 //The swap slot is on the disk
-        	set_page_writeback(page);
-                
-		bio = bio_alloc(GFP_NOIO, 1);
+                set_page_writeback(page);
+
+                bio = bio_alloc(GFP_NOIO, 1);
                 if (bio == NULL) {
                         set_page_dirty(page);
                         return -ENOMEM;
@@ -287,7 +324,7 @@ void (*end_write_func)(struct bio *, int))
                 if (bio) {
                         bio->bi_iter.bi_sector = offset;
                         bio->bi_iter.bi_sector <<= PAGE_SHIFT - 9;
-			bio->bi_bdev = sis->bdev;
+                        bio->bi_bdev = sis->bdev;
                         bio->bi_io_vec[0].bv_page = page;
                         bio->bi_io_vec[0].bv_len = PAGE_SIZE;
                         bio->bi_io_vec[0].bv_offset = 0;
@@ -303,66 +340,46 @@ void (*end_write_func)(struct bio *, int))
                 submit_bio(rw, bio);
                 goto out;
         }
-	
+
 	if(current == mempipe_dump_shm_thread) {
-		goto shmem;
-	}
-	if(sis->shm_end - sis->shm_start > SWAP_SHM_SIZE) {
-		if((dump_thread_finish == 0) && (all_pages_dumped == 1)) {
-			dump_thread_finish = 1;
-			wake_up_interruptible(&dump_thread_wait);
-		}
-	}
+		printk("********** goto shmem\n");
+                goto shmem;
+        }
+
+	if((dump_thread_running == 0) && dump_shm_should_run()){
+		printk("**********need shm_dump, dump_args = %p\n", dump_args);
+		//dump_args->start = sis->shm_start;
+		//dump_args->end = sis->shm_end;
+		dump_args->start = sis->disk_start;
+		dump_args->end = sis->disk_end;
+		printk("shm_start = %ld, shm_end = %ld, wake up shm_dump\n", dump_args->start, dump_args->end);
+		wake_up_interruptible(&dump_thread_wait);
+		printk("swap_writepage wait\n");
+		wait_event_interruptible(swap_thread_wait, swap_should_run());
+		printk("swap_writepage resume, shm_start = %ld, shm_end = %ld\n", sis->disk_start, sis->disk_end);
+	}	
 	
-	if(sis->shm_end - sis->shm_start > TOTAL_SWAP_SHM) {
+	/*	
+	if(!swap_should_run()){
 		wait_event_interruptible(swap_thread_wait, swap_should_run());
 	}
+	*/
 #endif
 
 shmem:
 	/*The swap slot is in shared memory*/
 	count_vm_event(PSWPOUT);
 
-	mdata_exist = (char *)sis->shm + OFFSET_1*PAGE_SIZE + offset*sizeof(char);
-	*mdata_exist = '0';
-	
-#ifdef PROACTIVE_SWAP
-	if(page->idx == 1) {
-		pgd_t *pgd;
-		pud_t *pud;
-		pmd_t *pmd;
-		
-		struct swapin_mdata *mdata = (struct swapin_mdata*)kmalloc(sizeof(struct swapin_mdata), GFP_KERNEL);
-		unsigned long address = page->rmap_addrs[0];
-		struct vm_area_struct *vma = page->rmap_vmas[0];
-
-		pgd = pgd_offset(vma->vm_mm, address);
-		pud = pud_offset(pgd, address);
-		pmd = pmd_offset(pud, address);
-
-		mdata->vma = vma;
-		mdata->pmd = pmd;
-		mdata->address = address;
-
-
-		*mdata_exist = '1';
-		memcpy((char *)sis->shm + OFFSET_2*PAGE_SIZE + offset*sizeof(struct swapin_mdata), (char *)mdata, sizeof(struct swapin_mdata));
-
-		kfree(mdata);
-
-	}
-#endif
-	
-	offset = get_offset(sis->mapper, offset);
-	memcpy((char *)sis->shm + offset*PAGE_SIZE, (char *)kmap(page), PAGE_SIZE);
-	printk("%s, offset = %ld\n", __func__, offset);
-
+	//unsigned long Nahanni_offset = get_offset(sis->mapper, offset);
+	unsigned long Nahanni_offset = offset + 1;
+	memcpy((char *)sis->shm + Nahanni_offset*PAGE_SIZE, (char *)kmap(page), PAGE_SIZE);
+	kunmap(page);
         unlock_page(page);
         //end_page_writeback(page);
 	if(offset > sis->shm_end) {
-		sis->shm_end++;
+		sis->shm_end = ++offset;
+		sis->disk_end = sis->shm_end;
 	}
-
 
 out:
 	return ret;
@@ -404,19 +421,22 @@ int mempipe_swap_readpage(struct page *page)
 	entry.val = page_private(page);
 	offset = swp_offset(entry);
 
-#ifdef SHM_DUMP	
+#ifdef SHM_DUMP 
+	if(offset == 0){
+		printk("read page, offset = %ld, shm_start = %ld\n", offset, sis->shm_start);
+        }
 	if(offset < sis->shm_start) {
-		bio = bio_alloc(GFP_KERNEL, 1);	
-		if (bio == NULL) {
-			unlock_page(page);
-			ret = -ENOMEM;
-			goto out;
-		}
+                bio = bio_alloc(GFP_KERNEL, 1);
+                if (bio == NULL) {
+                        unlock_page(page);
+                        ret = -ENOMEM;
+                        goto out;
+                }
 
-		if (bio) {
+                if (bio) {
                         bio->bi_iter.bi_sector = offset;
                         bio->bi_iter.bi_sector <<= PAGE_SHIFT - 9;
-			bio->bi_bdev = sis->bdev;
+                        bio->bi_bdev = sis->bdev;
                         bio->bi_io_vec[0].bv_page = page;
                         bio->bi_io_vec[0].bv_len = PAGE_SIZE;
                         bio->bi_io_vec[0].bv_offset = 0;
@@ -425,14 +445,19 @@ int mempipe_swap_readpage(struct page *page)
                         bio->bi_end_io = end_swap_bio_read;
                 }
 
-		count_vm_event(PSWPIN);
-		submit_bio(READ, bio);
-		goto out;
-	}
+                count_vm_event(PSWPIN);
+                submit_bio(READ, bio);
+                goto out;
+        }
 #endif
 
+
+
 	count_vm_event(PSWPIN);
+	//offset = get_offset(sis->mapper, offset);
+	offset = offset + 1;
 	memcpy((char *)kmap(page), (char *)sis->shm + offset*PAGE_SIZE, PAGE_SIZE);
+	kunmap(page);
 	SetPageUptodate(page);
 	unlock_page(page);
 	

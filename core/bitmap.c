3dd4ac598f9923f74b4dbd1ac77bee4193faecf8
qzhang90 <qzhang90@gatech.edu>
2016-08-23 23:30:08 -0400

    add bitmap

---
+++ core/bitmap.c
@@ -0,0 +1,74 @@
+#define BITS_PER_BYTE 8
+
+/*
+*Starting from the first bit of @*p, set the @idx bit (to 1). 
+*/
+void setbit(char *p, int idx){
+	int k1, k2;
+	char c;
+
+	k1 = idx/(BITS_PER_BYTE*sizeof(char)); /*How many bytes*/
+	k2 = idx%(BITS_PER_BYTE*sizeof(char)); /*How many bits left*/
+
+	c = *(p + k1);
+	*(p + k1) |= 1<<k2;
+		
+}
+
+/*
+*Starting from the first bit of @*p, clear the @idx bit (to 0). 
+*/
+void clearbit(char *p, int idx){
+	int k1, k2;
+        char c;
+
+        k1 = idx/(BITS_PER_BYTE*sizeof(char)); /*How many bytes*/
+        k2 = idx%(BITS_PER_BYTE*sizeof(char)); /*How many bits left*/
+
+	*(p + k1) &= ~(1 << k2);
+}
+
+/*
+*Starting from the first bit of @*p, clear all the @width bits
+*/
+void clear_all_bits(char *p, int width){
+	int i;
+
+	for(i = 0; i < width; i++){
+		clearbit(p, i);
+	}
+
+}
+
+/*
+*Starting from the first bit of @*p, find the first zero bit
+*At most @width bits will be checked
+*Return the index of that bit
+*/
+int first_zero_bit(char *p, int width){
+	int k1, k2, i, j, bit;
+	char c;
+
+	k1 = width/(BITS_PER_BYTE*sizeof(char)); /*How many bytes*/
+	k2 = width%(BITS_PER_BYTE*sizeof(char)); /*How many bits left*/
+
+
+
+	for(i = 0; i < k1; i++){
+		c = *(p + i);
+		for(j = 0; j < BITS_PER_BYTE; j++){
+			bit = (c>>j)&0x01;
+			if( bit == 0 )
+				return i*BITS_PER_BYTE + j;
+		}		
+	}
+
+	for(i = 0; i < k2; i++){
+		c = *(p + k1);
+		bit = (c>>i)&0x01;
+		if( bit == 0){
+			return k1*BITS_PER_BYTE + i;
+		}
+	}
+	return -1;
+}
---
+++ core/bitmap.h
@@ -0,0 +1,4 @@
+void setbit(char *p, int idx);
+void clearbit(char *p, int idx);
+void clear_all_bits(char *p, int width);
+int first_zero_bit(char *p, int width);
